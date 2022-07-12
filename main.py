e17db747617fc226ef9149fd4f152d7734e9070d
demagomes <me@ademar.uk>
2022-07-12 14:23:58 +0100

    added progress prints during test.

--- main.py
+++ main.py
@@ -24,16 +24,21 @@ def saveresultstocsv(download,upload,ping):
         writer.writerow({'timestamp':strftime("%Y-%m-%d %H:%M:%S", gmtime()),'download': download, 'upload': upload, 'ping': ping})   
  
 def speedTest():
+    print('Testing Internet Speeed...', end='\r')
     s = speedtest.Speedtest()
     servers = []
+    print('Getting Best Server.......', end='\r')
     s.get_servers(servers)
+    print('Running Speed Test........', end='\r')
     s.get_best_server()
     dmbps = s.download() / 1000000
     umbps = s.upload() / 1000000
+    print('Working  on results.......', end='\r')
     results_dict = s.results.dict()   
     printResults = 'Date: ' + time.asctime() + '|' + 'Download Speed (mbps): ' + str(round(dmbps)) + '|' + 'Upload Speed (mbps): ' + str(round(umbps)) + '|' + 'Ping: ' + str(results_dict["ping"])
     print(printResults)
-    saveresultstocsv(str(round(dmbps)),str(round(umbps)),str(results_dict["ping"]))  
+    saveresultstocsv(str(round(dmbps)),str(round(umbps)),str(results_dict["ping"])) 
+    print('Waiting for next run......', end='\r') 
    
 def printheader():
     print('Python Internet Speed Test')
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-A = int(input("enter 1st number"))
-B = int(input("enter 2nd number"))
-C = A + B
-print(C)
+# Prompts user to enter argument in terminal
+# A = int(input("enter 1st number"))
+# B = int(input("enter 2nd number"))
+# C = A + B
+# print(C)
+
+
+# Update same line in terminal 
+import time
+print('Test', end='\r')
+time.sleep(5)
+print('Test 2', end='\r')
