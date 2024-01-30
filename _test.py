7a0566b8c229d96e7ce9aefc21687aae93adb395
Ademar Gomes <me@ademar.uk>
2024-01-30 13:27:44 +0000

    added unit test for cprint function

--- _test.py
+++ _test.py
@@ -1,8 +1,12 @@
 import main
 
-# First test (WIP)
+# Test printheader() output
 def test_print_header(capsys):
+
+    # Executes function
     main.printheader()
+
+    # captures the output of the function using capsys from pytest
     captured = capsys.readouterr()
     all_outputs = captured.out.split('\n')
 
@@ -10,7 +14,23 @@ def test_print_header(capsys):
     assert len(all_outputs) == 4
 
     # test each row's content
-    assert all_outputs[0] == "\x1b[95mPython Internet Speed Test\x1b[0m"
-    assert all_outputs[1] == "\x1b[92mhttps://github.com/demagomes/pyinternetspeed\x1b[0m"
-    assert all_outputs[2] == "\x1b[92mPlease press Control+C to end the program\x1b[0m"
-    assert all_outputs[3] == ""
+    assert all_outputs[0] == '\x1b[95mPython Internet Speed Test\x1b[0m'
+    assert all_outputs[1] == '\x1b[92mhttps://github.com/demagomes/pyinternetspeed\x1b[0m'
+    assert all_outputs[2] == '\x1b[92mPlease press Control+C to end the program\x1b[0m'
+    assert all_outputs[3] == ''
+
+# Test cprint function output
+def test_cprint(capsys):
+
+    # Executes function
+    main.cprint('Unit Test Header','HEADER')
+
+    # captures the output of the function using capsys from pytest
+    captured = capsys.readouterr()
+
+     # test output
+    assert captured.out== '\x1b[95mUnit Test Header\x1b[0m\n'
+
+
+# \x1b[95mUnit Test Header\x1b[0m\n
+# \x1b[95mUnit Test Header\x1b[0m\n
